6a080a742edeba7e83740d69684c5601dc99ca2d
creatorosai-dev <waghmareg244@gmail.com>
2026-07-08 12:03:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 document.querySelector("button").addEventListener("click", function () {
     alert("Welcome to CreatorOS AI 🚀");
 });
+
+function askAI(){
+
+    let question =
+    document.getElementById("userInput").value;
+
+    let answer =
+    document.getElementById("response");
+
+    if(question==""){
+        answer.innerHTML="Please enter a question.";
+    }
+    else{
+        answer.innerHTML=
+        "🤖 CreatorOS AI: I received your question: " + question;
+    }
+}
